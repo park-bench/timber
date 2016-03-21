ab9e6cc24b96ac00f86bb655b546e6265c09dcd7
Andrew Klapp <lithium@null.net>
2016-03-21 12:20:49 -0500

    Finally made the packaging sane.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import setuptools
+
+setuptools.setup(name='timber',
+    version='0.1',
+    description='A simple logging library.',
+    author='Joel Allen Luellwitz and Andrew Klapp',
+    author_email='eviljoel@linux.com, andklapp@null.net',
+    license='GPLv3',
+    packages=['timber'],
+    zipsafe=False)
